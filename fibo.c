6270226674f6c73a7da81bc5b47e4e9dc41c80c9
Hugo Araya Carrasco <hugo.araya.carrasco@gmail.com>
2026-05-28 10:22:33 -0400

    Ejercicios

---
+++ fibo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int n, i = 0;
+    int t1 = 0, t2 = 1, siguienteTermino;
+    printf("Ingrese un numero entero: ");
+    scanf("%f", &n);
+    // Aqui va el mientras 
+    while (i <= n){
+        siguienteTermino = t1 + t2;
+        t1 = t2;
+        t2 = siguienteTermino;
+        i++;
+    }
+    printf("El Fibonacci de %d es %d\n", n, t2);
+    return 0;
+}
